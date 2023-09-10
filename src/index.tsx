db87b8a0ddc3a52a8ba985a0055712292c638a4d
thumbthing <ilscc0902@gmail.com>
2023-09-11 05:39:09 +0900

    Feat: msw 세팅 추가(init, index.tsx 적용, handler 생성, browser 환경 세팅)

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,43 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function prepare() {
+  if (process.env.NODE_ENV === 'development') {
+    import('./mocks/browser').then((worker) => {
+      worker.default.start();
+    });
+  }
+  return Promise.resolve();
+}
+
+async function main() {
+  if (process.env.NODE_ENV === 'development') {
+    if (window.location.pathname === '/graph') {
+      window.location.pathname = '/graph/';
+      return null;
+    }
+
+    import('./mocks/browser').then((worker) => {
+      worker.default.start({
+        serviceWorker: {
+          url: '/graph/mockServiceWorker.js',
+        },
+      });
+    });
+  }
+  return null;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+
+prepare()
+  .then(() => {
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+  })
+  .then(() => main());
 
 reportWebVitals();
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,8 @@
 import { rest } from 'msw';
 import mockData from './mockData.json';
-import jsonType from '../types/json.type';
 
 const handlers = [
-  rest.get<jsonType[]>('/graph', (req, res, ctx) => {
+  rest.get('http://localhost:4000/graph', (req, res, ctx) => {
     const data = mockData;
 
     return res(ctx.status(200), ctx.json(data));
