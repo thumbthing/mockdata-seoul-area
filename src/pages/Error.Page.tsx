1b771e2e4ed69d7448a6b34c224d4944a0d808e7
thumbthing <ilscc0902@gmail.com>
2023-09-10 21:59:14 +0900

    Feat: page 생성(Error, Graph)

---
+++ src/pages/Error.Page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ErrorPage() {
+  return <div>Error.page</div>;
+}
+
+export default ErrorPage;
---
+++ src/pages/Graph.Page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function GraphPage() {
+  return <div>Graph.page</div>;
+}
+
+export default GraphPage;
