3af0f6cb4cf9b13a259354d64044c7cef8605dbf
thumbthing <ilscc0902@gmail.com>
2023-09-11 07:23:05 +0900

    Feat: context (state, service) 테스트

--- src/pages/Graph.Page.tsx
+++ src/pages/Graph.Page.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { MockContext } from '../context/State.Context';
+import { FunctionContext } from '../context/Service.context';
+import errorHandler from '../utils/errorHandler';
 
 function GraphPage() {
-  return <div>Graph.page</div>;
+  const { state, setState } = MockContext();
+  const { getData, dataParser, getFilteredData } = FunctionContext();
+  const { GraphData, SelectedDataKey, SelectedDataValue } = state;
+
+  const handleClick = async () => {
+    try {
+      const mockData = await getData();
+      if (mockData) {
+        const rawData = mockData.data.response;
+        const parsedData = await dataParser(rawData);
+        setState((prevState) => ({ ...prevState, GraphData: parsedData }));
+      }
+    } catch (error) {
+      errorHandler(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log('GraphDate : ', GraphData);
+    console.log('SelectedDataKey : ', SelectedDataKey);
+    console.log('SelectedValue : ', SelectedDataValue);
+    console.log('fn : 1 ', getData);
+    console.log('fn : 2 ', dataParser);
+    console.log('fn : 3 ', getFilteredData);
+  }, []);
+
+  useEffect(() => {
+    console.log('GraphData from mock : ', GraphData);
+  }, [GraphData]);
+
+  return (
+    <>
+      <div>GraphPage</div>
+      <div>
+        <button type="button" onClick={handleClick}>
+          get data
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default GraphPage;
