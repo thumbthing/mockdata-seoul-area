f23aaab9edfa59d9270d48f9ee6549dd89cf2858
thumbthing <ilscc0902@gmail.com>
2023-09-12 13:48:22 +0900

    Feat: 그래프 bar 요소 생성

---
+++ src/components/ValueBar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import { MockContext } from '../context/State.Context';
+
+const Bar = styled.div<{ height: number }>`
+  width: 10px;
+  height: ${(props) => (props.height ? `${props.height}px` : 0)};
+  border: 1px solid white;
+  background-color: grey;
+  margin: 1px;
+`;
+
+interface ValueBarProps {
+  index: number;
+}
+
+function ValueBar({ index }: ValueBarProps) {
+  const { state } = MockContext();
+  const { GraphData } = state;
+
+  const singleData = GraphData[index];
+  const caculateHeight = (valueBar: number) => {
+    const height = Math.round(valueBar / 50);
+    return height;
+  };
+  const height = caculateHeight(singleData.value_bar);
+
+  return <Bar height={height}> </Bar>;
+}
+
+export default ValueBar;
