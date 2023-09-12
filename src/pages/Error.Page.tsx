c4218421d0ee402d828feb64f7b991adb3dfe49e
thumbthing <ilscc0902@gmail.com>
2023-09-13 07:53:42 +0900

    Feat: 에러 페이지 요소 생성, 일정 시간 이후 graph 페이지 리다이렉트 기능 추가

--- src/pages/Error.Page.tsx
+++ src/pages/Error.Page.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function ErrorPage() {
-  return <div>Error.page</div>;
+  const [second, setSecond] = useState(5);
+
+  const navigate = useNavigate();
+
+  const redirectToGraphPage = useCallback(() => {
+    setTimeout(() => {
+      navigate('/graph');
+    }, 5000);
+  }, []);
+
+  const timer = useCallback(() => {
+    setTimeout(() => {
+      setSecond(second - 1);
+    }, 1000);
+  }, [second]);
+
+  useEffect(() => {
+    timer();
+    redirectToGraphPage();
+  }, [second]);
+
+  return (
+    <div>
+      <div>
+        <h1>잘못된 경로로 접속하셨습니다</h1>
+      </div>
+      <div>
+        <h3>잠시만 기다려 주세요</h3>
+        <h3>{second}초 후에 Graph 페이지로 이동합니다</h3>
+      </div>
+    </div>
+  );
 }
 
 export default ErrorPage;
